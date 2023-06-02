fe3d45724a3f0bf593ce00fb7c6d1dcd7c5ec35e
napetico <enabaro@gmail.com>
2023-06-02 18:36:25 +0200

    Add message components and structure

---
+++ script.js
@@ -0,0 +1,13 @@
+// This is the message structure
+/* Everytime I was [randomFeeling] 
+my [familyMember] used to say "[sayingPartOne] + [sayingPartTwo]". 
+Such wise words.*/
+
+// Message components as arrays inside and object
+
+const funkyMessage = {
+    randomFeeling: ['feeling sad', 'being lazy', 'being dishonest', 'holding anger', 'acting like a jerk', 'felling like giving up'],
+    familyMember: ['grandpa', 'grandma', 'dad', 'mom', 'dog'],
+    sayingPartOne: ['Any job worth doing', 'When the going gets tough', 'Don\'t count your chickens', 'One man\'s trash', 'The squeaky wheel', 'Woof, woof, arf'],
+    sayingPartTwo: [' is worth doing well.', ', the tough get going.', ' leave no stone unturned.', ' before they\'ve hatched.', ' is another man\'s treasure.', ' flock together.']
+};
